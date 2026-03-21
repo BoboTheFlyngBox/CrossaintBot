ce4aab2eeb38c9d2a02eecb6f5d3e5ba59706729
lazycaps <135090883+lazycaps@users.noreply.github.com>
2026-03-21 13:18:47 -0500

    add website upload to /import

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ const MAX_TIME = {
   6: 30 * 60 * 1000,
 };
 
+const WEBSITE_URL = process.env.WEBSITE_URL
+const WEBSITE_API_KEY = process.env.WEBSITE_API_KEY
+
 function mkS() {
   return { settings: { leagueLimits: { ...MAX_TIME }, pendingDisplacements: {}, loggingEnabled: true }, channels: {} };
 }
@@ -1359,6 +1362,41 @@ client.on('interactionCreate', async (interaction) => {
 
       if(result.missing.length > 0){
         lines.push(`Unmatched MCSR names: ${result.missing.join(', ')}`);
+      }
+      
+      const results = [];
+      i = 1
+      for (const player of result.matched){
+        results.push({
+          playerName: player.name.split("(")[0],
+          pointsWon: player.dnf ? 0 : gRC(competition) - i + 1,
+          timeMs: player.timeMs,
+          placement: i,
+        })
+        i++
+      }
+      
+      try {
+        const request = await fetch(WEBSITE_URL + "/api/write/match", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": WEBSITE_API_KEY
+        },
+        body: JSON.stringify({
+          "weekNumber": Number(competition.week),
+          "matchNumber": Number(seed.name),
+          "leagueTier": Number(competition.leagueNumber),
+          "rankedMatchId": matchId,
+          "results": results,
+        }),
+      });
+        if (!request.ok) {
+          throw new Error(`${request.status} error, could not import match data to website.`);
+        }
+      } catch (error) {
+        console.log(error.stack);
+        throw new Error(`${error.message} - Failed to post match ${matchId}`);
       }
 
       await interaction.editReply(lines.join('\n'));
